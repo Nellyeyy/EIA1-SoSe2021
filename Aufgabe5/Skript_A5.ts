ce5ce62e69b85eb3c81de3c02a8372264add276f
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-04-20 21:40:44 +0200

    Aufgabe5 fertig

--- Aufgabe5/Skript_A5.ts
+++ Aufgabe5/Skript_A5.ts
@@ -17,22 +17,33 @@ var au_18: number = 2100.5;
 var au_08: number = 1993;
 
 var ges_18: number = eu_18 + af_18 + sa_18 + na_18 + as_18 + au_18;
-var ges_af_18: number = af_18 * 100 / ges_18;
-var ges_sa_18: number = sa_18 * 100 / ges_18;
-var ges_eu_18: number = eu_18 * 100 / ges_18;
-var ges_na_18: number = na_18 * 100 / ges_18;
-var ges_as_18: number = as_18 * 100 / ges_18;
-var ges_au_18: number = au_18 * 100 / ges_18;
 
-var ges_08: number = eu_08 + af_08 + sa_08 + na_08 + as_08 + au_08;
-var ges_af_08: number = af_08 * 100 / ges_08;
-var ges_sa_08: number = sa_08 * 100 / ges_08;
-var ges_eu_08: number = eu_08 * 100 / ges_08;
-var ges_na_08: number = na_08 * 100 / ges_08;
-var ges_as_08: number = as_08 * 100 / ges_08;
-var ges_au_08: number = au_08 * 100 / ges_08;
+console.log('Die Emission von Afrika ist: ' + af_18 + ' kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit ' + af_18*100/ges_18 + ' %');
+console.log('Für Afrika hat sich 2018 im Vergleich zu 2008 die Emmission um ' + (af_18-af_08)/af_08*100 + ' verändert');
+console.log('2018 im Vergleich zu 2008 sind das ' + (af_18-af_08) + ' kg CO2');
 
+console.log('Die Emission von Süd Amerika ist: ' + sa_18 + ' kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Süd Amerika damit ' + sa_18*100/ges_18 + ' %');
+console.log('Für Süd Amerika hat sich 2018 im Vergleich zu 2008 die Emmission um ' + (sa_18-sa_08)/sa_08*100 + ' verändert');
+console.log('2018 im Vergleich zu 2008 sind das ' + (sa_18-sa_08) + ' kg CO2');
 
+console.log('Die Emission von Süd Amerika ist: ' + eu_18 + ' kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Süd Amerika damit ' + eu_18*100/ges_18 + ' %');
+console.log('Für Süd Amerika hat sich 2018 im Vergleich zu 2008 die Emmission um ' + (eu_18-eu_08)/eu_08*100 + ' verändert');
+console.log('2018 im Vergleich zu 2008 sind das ' + (eu_18-eu_08) + ' kg CO2');
 
-console.log('Die Emission von Afrika ist: 1235.5 kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit ' + ges_af_18 + ' %');
+console.log('Die Emission von Süd Amerika ist: ' + na_18 + ' kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Süd Amerika damit ' + na_18*100/ges_18 + ' %');
+console.log('Für Süd Amerika hat sich 2018 im Vergleich zu 2008 die Emmission um ' + (na_18-na_08)/na_08*100 + ' verändert');
+console.log('2018 im Vergleich zu 2008 sind das ' + (na_18-na_08) + ' kg CO2');
+
+console.log('Die Emission von Süd Amerika ist: ' + as_18 + ' kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Süd Amerika damit ' + as_18*100/ges_18 + ' %');
+console.log('Für Süd Amerika hat sich 2018 im Vergleich zu 2008 die Emmission um ' + (as_18-as_08)/as_08*100 + ' verändert');
+console.log('2018 im Vergleich zu 2008 sind das ' + (as_18-as_08) + ' kg CO2');
+
+console.log('Die Emission von Süd Amerika ist: ' + au_18 + ' kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Süd Amerika damit ' + au_18*100/ges_18 + ' %');
+console.log('Für Süd Amerika hat sich 2018 im Vergleich zu 2008 die Emmission um ' + (au_18-au_08)/au_08*100 + ' verändert');
+console.log('2018 im Vergleich zu 2008 sind das ' + (au_18-au_08) + ' kg CO2');
