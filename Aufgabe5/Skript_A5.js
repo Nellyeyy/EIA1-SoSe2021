5640c69fac26689bb802d06922be7db30b9a18a9
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-04-20 20:08:10 +0200

    Aufgabe5

--- Aufgabe5/Skript_A5.js
+++ Aufgabe5/Skript_A5.js
@@ -1,2 +1,5 @@
-console.log("Hallo Welt");
+var num1 = 20;
+var num2 = 12;
+var result = num1 + num2;
+alert(result);
 //# sourceMappingURL=Skript_A5.js.map
--- Aufgabe5/Skript_A5.ts
+++ Aufgabe5/Skript_A5.ts
@@ -1,1 +1,6 @@
-console.log("Hallo Welt");
+var num1: number = 20;
+var num2: number = 12;
+
+var result: number= num1 + num2;
+
+alert(result);
