382c1dce1dcab309c6b4518f0cd1e9315cd37cd8
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-06-02 14:21:38 +0200

    Aufgabe 11 Teil 1

---
+++ Aufgabe11/script11.js
@@ -0,0 +1,93 @@
+// Varriablen
+var inputDOMElement;
+var addButtonDOMElement;
+var todosDOMElement;
+var counterDOMElement;
+var counterDOMElementOpen;
+var counterDOMElementDone;
+//Array
+var tDA = [
+    {
+        todosText: "Lorem",
+        todosChecked: true
+    },
+    {
+        todosText: "Ipsum",
+        todosChecked: false
+    },
+    {
+        todosText: "Dolor",
+        todosChecked: false
+    }
+];
+window.addEventListener("load", function () {
+    inputDOMElement = document.querySelector("#inputTodo");
+    addButtonDOMElement = document.querySelector("#addButton");
+    todosDOMElement = document.querySelector("#todos");
+    counterDOMElement = document.querySelector("#counter");
+    counterDOMElementOpen = document.querySelector("#open");
+    counterDOMElementDone = document.querySelector("#done");
+    addButtonDOMElement.addEventListener("click", addTodo);
+    drawListToDOM();
+    function drawListToDOM() {
+        todosDOMElement.innerHTML = "";
+        var _loop_1 = function (index) {
+            var todo = document.createElement("div");
+            todo.classList.add("todo");
+            todo.innerHTML = "<span class='check " + tDA[index].todosChecked + "'><i class='fas fa-check'></i></span>"
+                + tDA[index].todosText +
+                "<span class='trash fas fa-trash-alt'></span>";
+            todo.querySelector(".check").addEventListener("click", function () {
+                toggleCheckState(index);
+            });
+            todo.querySelector(".trash").addEventListener("click", function () {
+                deleteTodo(index);
+            });
+            todosDOMElement.appendChild(todo);
+        };
+        for (var index = 0; index < tDA.length; index++) {
+            _loop_1(index);
+        }
+        updateCounter();
+        updateOpen();
+        updateDone();
+    }
+    function updateCounter() {
+        counterDOMElement.innerHTML = tDA.length + " in total";
+    }
+    function updateOpen() {
+        var open = 0;
+        for (var index = 0; index < tDA.length; index++) {
+            if (tDA[index].todosChecked == false)
+                open++;
+        }
+        counterDOMElementOpen.innerHTML = open + " open,";
+    }
+    function updateDone() {
+        var done = 0;
+        for (var index = 0; index < tDA.length; index++) {
+            if (tDA[index].todosChecked == true)
+                done++;
+        }
+        counterDOMElementDone.innerHTML = done + " done";
+    }
+    function addTodo() {
+        if (inputDOMElement.value != "") {
+            tDA.unshift({
+                todosText: inputDOMElement.value,
+                todosChecked: false
+            });
+            inputDOMElement.value = "";
+            drawListToDOM();
+        }
+    }
+    function toggleCheckState(index) {
+        tDA[index].todosChecked = !tDA[index].todosChecked;
+        drawListToDOM();
+    }
+    function deleteTodo(index) {
+        tDA.splice(index, 1);
+        drawListToDOM();
+    }
+});
+//# sourceMappingURL=script11.js.map
---
+++ Aufgabe11/script11.ts
@@ -0,0 +1,139 @@
+// Varriablen
+var inputDOMElement: HTMLInputElement;
+var addButtonDOMElement: HTMLElement;
+var todosDOMElement: HTMLElement;
+var counterDOMElement: HTMLElement;
+var counterDOMElementOpen: HTMLElement;
+var counterDOMElementDone: HTMLElement;
+
+//interface
+interface ToDos {
+    todosText: string;
+    todosChecked: boolean;
+}
+
+//Array
+let tDA: ToDos[] = [
+    {
+        todosText: "Lorem",
+        todosChecked: true
+    },
+    {
+        todosText: "Ipsum",
+        todosChecked: false
+    },
+    {
+        todosText: "Dolor",
+        todosChecked: false
+    }
+];
+
+window.addEventListener("load", function (): void {
+
+
+    inputDOMElement = document.querySelector("#inputTodo");
+    addButtonDOMElement = document.querySelector("#addButton");
+    todosDOMElement = document.querySelector("#todos");
+    counterDOMElement = document.querySelector("#counter");
+    counterDOMElementOpen = document.querySelector("#open");
+    counterDOMElementDone = document.querySelector("#done");
+
+
+    addButtonDOMElement.addEventListener("click", addTodo);
+
+
+    drawListToDOM();
+
+
+    function drawListToDOM(): void {
+
+        todosDOMElement.innerHTML = "";
+
+
+        for (let index: number = 0; index < tDA.length; index++) {
+
+
+            let todo: HTMLElement = document.createElement("div");
+            todo.classList.add("todo");
+
+            todo.innerHTML = "<span class='check " + tDA[index].todosChecked + "'><i class='fas fa-check'></i></span>"
+                + tDA[index].todosText +
+                "<span class='trash fas fa-trash-alt'></span>";
+
+            todo.querySelector(".check").addEventListener("click", function (): void {
+
+                toggleCheckState(index);
+            });
+            todo.querySelector(".trash").addEventListener("click", function (): void {
+
+                deleteTodo(index);
+            });
+
+            todosDOMElement.appendChild(todo);
+
+        }
+
+        updateCounter();
+        updateOpen();
+        updateDone();
+
+    }
+
+    function updateCounter(): void {
+        counterDOMElement.innerHTML = tDA.length + " in total";
+    }
+
+    function updateOpen(): void {
+        let open: number = 0;
+        for (var index: number = 0; index < tDA.length; index++) {
+            if (tDA[index].todosChecked == false)
+                open++;
+        }
+        counterDOMElementOpen.innerHTML = open + " open,";
+
+
+    }
+
+    function updateDone(): void {
+        let done: number = 0;
+        for (var index: number = 0; index < tDA.length; index++) {
+            if (tDA[index].todosChecked == true)
+                done++;
+        }
+        counterDOMElementDone.innerHTML = done + " done";
+
+
+    }
+
+    function addTodo(): void {
+
+        if (inputDOMElement.value != "") {
+
+            tDA.unshift({
+
+                todosText: inputDOMElement.value,
+
+
+                todosChecked: false
+            });
+
+            inputDOMElement.value = "";
+
+            drawListToDOM();
+        }
+    }
+
+    function toggleCheckState(index: number): void {
+
+        tDA[index].todosChecked = !tDA[index].todosChecked;
+
+        drawListToDOM();
+    }
+
+    function deleteTodo(index: number): void {
+
+        tDA.splice(index, 1);
+
+        drawListToDOM();
+    }
+});
