1c0f2db515dcb410a3aa7b26bbff200c658a360e
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-06-09 16:08:51 +0200

    Endaufgabe anfang

---
+++ Endaufgabe/ttt-script.js
@@ -0,0 +1,14 @@
+// Variablen
+var leicht;
+var mittel;
+var schwer;
+window.addEventListener("load", function () {
+    leicht = document.querySelector("#leicht");
+    mittel = document.querySelector("#mittel");
+    schwer = document.querySelector("#schwer");
+    // document.querySelector("leicht").addEventListener("click", function (): void { new (); });
+    // function new (String): void {
+    //     console.log("leicht");
+    // }
+});
+//# sourceMappingURL=ttt-script.js.map
---
+++ Endaufgabe/ttt-script.ts
@@ -0,0 +1,24 @@
+// Variablen
+var leicht: HTMLElement;
+var mittel: HTMLElement;
+var schwer: HTMLElement;
+
+
+window.addEventListener("load", function (): void {
+
+    leicht = document.querySelector("#leicht");
+    mittel = document.querySelector("#mittel");
+    schwer = document.querySelector("#schwer");
+    
+    
+
+    // document.querySelector("leicht").addEventListener("click", function (): void { new (); });
+
+    // function new (String): void {
+    //     console.log("leicht");
+
+    // }
+
+
+
+});
