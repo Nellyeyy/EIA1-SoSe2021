2aa4be7c64db3a839c4386944a258cb3153bbdac
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-06-11 13:57:00 +0200

    endaufgabe

--- Endaufgabe/ttt-script.js
+++ Endaufgabe/ttt-script.js
@@ -3,29 +3,39 @@ var leicht;
 var mittel;
 var schwer;
 var spielfeld;
+var oben;
+var mitte;
+var unten;
 window.addEventListener("load", function () {
     // Zuweisungen
     leicht = document.querySelector("#leicht");
     mittel = document.querySelector("#mittel");
     schwer = document.querySelector("#schwer");
     spielfeld = document.querySelector("#spielfeld");
+    oben = document.querySelector("#oben");
+    mitte = document.querySelector("#mitte");
+    unten = document.querySelector("#unten");
     //Spielfeld nicht sichtbar bei beim aufrufen
     // spielfeld.innerHTML = "";
+    // oben.innerHTML = "";
     // Auswahl der Schwirigkeit
     leicht.addEventListener("click", playleicht);
     function playleicht() {
-        console.log("Sie spielen die leichte Version");
-        // if (spielfeld.innerHTML = !"") {
-        //     spielfeld
-        // };
+        console.log("Sie spielen ein leichtes Spiel!");
+        // if (spielfeld.value != "") {
+        //     console.log ("Test");
+        // }
+        // if (oben.value != "") {
+        //     console.log ("Test");
+        // }
     }
     mittel.addEventListener("click", playmittel);
     function playmittel() {
-        console.log("Sie spielen die mittlere Version");
+        console.log("Sie spielen ein mittleres Spiel!");
     }
     schwer.addEventListener("click", playschwer);
     function playschwer() {
-        console.log("Sie spielen die schwere Version");
+        console.log("Sie spielen ein schweres Spiel!");
     }
 });
 //# sourceMappingURL=ttt-script.js.map
--- Endaufgabe/ttt-script.ts
+++ Endaufgabe/ttt-script.ts
@@ -3,7 +3,10 @@ var leicht: HTMLElement;
 var mittel: HTMLElement;
 var schwer: HTMLElement;
 
-var spielfeld: HTMLElement;
+var spielfeld: HTMLInputElement;
+var oben: HTMLInputElement;
+var mitte: HTMLInputElement;
+var unten: HTMLInputElement;
 
 window.addEventListener("load", function (): void {
 
@@ -13,31 +16,42 @@ window.addEventListener("load", function (): void {
     schwer = document.querySelector("#schwer");
 
     spielfeld = document.querySelector("#spielfeld");
+    oben = document.querySelector("#oben");
+    mitte = document.querySelector("#mitte");
+    unten = document.querySelector("#unten");
 
     //Spielfeld nicht sichtbar bei beim aufrufen
     // spielfeld.innerHTML = "";
+    // oben.innerHTML = "";
+
 
     // Auswahl der Schwirigkeit
     leicht.addEventListener("click", playleicht);
 
     function playleicht(): void {
-        console.log("Sie spielen die leichte Version");
-        // if (spielfeld.innerHTML = !"") {
-        //     spielfeld
-        // };
+        console.log("Sie spielen ein leichtes Spiel!");
+
+        // if (spielfeld.value != "") {
+        //     console.log ("Test");
+        // }
+
+        // if (oben.value != "") {
+        //     console.log ("Test");
+            
+        // }
     }
 
     mittel.addEventListener("click", playmittel);
 
     function playmittel(): void {
-        console.log("Sie spielen die mittlere Version");
+        console.log("Sie spielen ein mittleres Spiel!");
     }
 
     schwer.addEventListener("click", playschwer);
 
     function playschwer(): void {
-        console.log("Sie spielen die schwere Version");
+        console.log("Sie spielen ein schweres Spiel!");
     }
 
-
 });
+
